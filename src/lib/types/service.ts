1d35c25609f06131bcddba2ede3069e0f10d436d
ovi2 <obi2@ovihost.local>
2026-02-12 18:24:18 +0100

    Clean service.ts

--- src/lib/types/service.ts
+++ src/lib/types/service.ts
@@ -2,10 +2,3 @@ export interface ServiceClientConfig {
   baseUrl: string;
   prefix: string;
 }
-
-export interface FileEntry {
-  name: string;
-  path: string;
-  isDir: boolean;
-  size?: number;
-}
